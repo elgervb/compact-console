a43263b4b6e6b84a0a8b873fc482309762311cae
elger <elgervb@gmail.com>
2015-04-16 23:39:41 +0200

    request input and run commands

--- run.php
+++ run.php
@@ -4,5 +4,17 @@ include 'lib/compact.phar';
 include './Console.php';
 
 $c = new Console();
-$input = $c->input("Your message");
-$c->writeln($input);
+$c->writeln("PHP Console");
+
+// create endless loop and receive commands
+while (true){
+    $input = $c->input("", "exit");
+    
+    // check if we should exit
+    if ($input === "exit" || $input === "quit"){
+        exit();
+    }
+    
+    // run the command
+    $c->run($input);
+}
