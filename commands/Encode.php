b914cf2f3173bcdf8a3ab3b57f3410023362b313
elger <elgervb@gmail.com>
2015-04-17 22:01:59 +0200

    new command base64 encode a file

--- commands/Encode.php
+++ commands/Encode.php
@@ -16,4 +16,20 @@ class Encode
         
         $this->console->writeln(base64_encode($content));
     }
+    
+    public function base64file($file, $exportFile=null){
+        if (is_file($file)){
+            if (!$exportFile){
+                $exportFile = $file . ".base64";
+            }
+        
+            $this->console->writeln("Exporting file " . $exportFile);
+            $fp = fopen( $exportFile, "wb");
+            fwrite($fp, base64_encode(file_get_contents($file)));
+            fclose($fp);
+        }
+        else{
+            $this->console->writeln($file . " is not a valid file. Please check path.");
+        }
+    }
 }
